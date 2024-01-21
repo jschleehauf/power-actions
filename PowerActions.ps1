cb1ff984ea7508a31cf1657c407d42292d3e9c76
jschleehauf <49009002+jschleehauf@users.noreply.github.com>
2024-01-21 16:41:50 -0700

    Update PowerActions.ps1
    Added error handling code.

--- PowerActions.ps1
+++ PowerActions.ps1
@@ -2,9 +2,18 @@
 # Last Edit: 01/05/2024
 # This PowerShell script allows a user with admin credentials to run Configuration Manager Actions
 # on a specified machine that is connected to the domain. 
+# Version 1
+# This version wraps the code in a try and catch block for error handling.
 
+# set variable for errors
+$retry
+
+
+do {
+	try {
 # Prompt the user for the computer name
 $computerName = Read-Host "Enter the computer name"
+
 
 # store all the actions to run
 $actions = 
@@ -38,3 +47,11 @@ Invoke-WMIMethod -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{0
 
 # Run the actions on the specified computer
 Invoke-Command -ComputerName $computerName -ScriptBlock $actions
+
+# Change variable to false since execution successful.
+$retry = false
+}
+	catch {
+		$retryCount++
+	}
+} while ($retry)
